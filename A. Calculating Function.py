cd617669d576ec5d5c9bec82084746376fdc063d
TanTheTiger <157776517+TanTheTiger@users.noreply.github.com>
2025-01-23 14:44:09 +0530

    Optimized A. Calculating Function.py
    Verdict: "Accepted"
    Time: 92 ms
    Memory: 0 KB

--- A. Calculating Function.py
+++ A. Calculating Function.py
@@ -1,8 +1,5 @@
 n = int(input())
-o = 0
-for i in range(1,n+1):
-    if i%2 != 0:
-        o-=i
-    else:
-        o+=i
-print(o)
+if n%2 != 0:
+    print(int(-((n+1)/2)))
+else:
+    print(int(n/2))
