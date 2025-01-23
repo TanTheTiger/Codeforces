20dddb1f49ea4a806fae3bd0037d123fff02d23c
TanTheTiger <157776517+TanTheTiger@users.noreply.github.com>
2025-01-23 14:25:07 +0530

    Optimized A. Magnets.py
    Verdict: "Accepted"
    Time: 342 ms
    Memory: 0 KB

--- A. Magnets.py
+++ A. Magnets.py
@@ -1,15 +1,9 @@
 n = int(input())
-s = []
 t = 1
 for i in range(n):
     j = list(input())
-    s.append(j[0])
-    s.append(j[1])
-s.pop(0)
-s.pop(-1)
-while len(s) != 0:
-    if s[0] == s[1]:
-        t+=1
-    s.pop(1)
-    s.pop(0)
+    if i != 0:
+        if j[0] == p:
+            t+=1
+    p = j[1]
 print(t)
