80b7f057316ec07016d1a7901b44a31887dcebda
TanTheTiger <tan.leaningtree@gmail.com>
2025-03-21 20:18:35 +0530

    Updated Divisibility Problem.py

--- A. Divisibility Problem(INC).py
+++ A. Divisibility Problem(INC).py
@@ -4,6 +4,11 @@ for i in range(t):
     b = int(a[1])
     a = int(a[0])
     c = a
-    while a%b != 0:
-        a+=1
-    print(a-c)
+    if a < b:
+        print(b-a)
+    elif a == b:
+        print(a)
+    else:
+        while a%b != 0:
+            a+=1
+        print(a-c)
